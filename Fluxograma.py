0943cd6fc517d92a20c66d9f8a2d442e15d69eae
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-08-22 21:57:58 -0300

    FLuxograma em diagrama

--- Fluxograma.py
+++ Fluxograma.py
@@ -1,11 +1,21 @@
 numero = int(input("Qual o numero?"))
 
+# numero = int(numero)
+
 def verificacaoEntrada(numero):
+
     if numero > 0:
+
         return "Numero maior que 0"
+    
     elif numero < 0:
+
         return "Numero menor que 0"
+    
     else:
+
         return "Igual a 0"
+    
 verificacaoEntrada(numero)
+
 print(verificacaoEntrada(numero))
