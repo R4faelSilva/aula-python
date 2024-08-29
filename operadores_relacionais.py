f52ff7c56e21b8362d150ac9f4b23a5eb5e946f8
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-08-28 22:52:19 -0300

    Operadores Relacionais

--- operadores_relacionais.py
+++ operadores_relacionais.py
@@ -3,24 +3,31 @@ b = 9
 
 # Igualdade (==)
 resultado = a == b
-print(resultado)
+print("###########################")
+print("")
+print(f"{a} e {b}")
+print("")
+
+print(f"Igualdade: {resultado}")
 
 # Diferença (!=)
 resultado = a != b
-print(resultado)
+print(f"Diferença: {resultado}")
 
 # Maior que (>)
 resultado = a > b
-print(resultado)
+print(f"Maior que: {resultado}")
 
 # Menor que (<)
 resultado = a < b
-print(resultado)
+print(f"Menor que: {resultado}")
 
 # Maior ou igual (>=)
 resultado = a >= b
-print(resultado)
+print(f"Menor ou igual: {resultado}")
 
 # Menor ou igual (<=)
 resultado = a <= b
-print(resultado)
+print(f"Menor ou igual: {resultado}")
+print("")
+print("###########################")
