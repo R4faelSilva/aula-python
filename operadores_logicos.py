9fc8a077f0c5c18933af0a6f43d08f758c383a09
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-08-29 21:54:16 -0300

    Teste de Mesa Excel

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -5,24 +5,24 @@ verf = True
 
 dia = int(input("Que dia é hoje?"))
 idade = int(input("Qual a idade?"))
-listaEspera = int(input("Está na lista de espera?"))
-indicacao = int(input("Tem indicação?"))
+listaEspera = input("Está na lista de espera?")
+indicacao = input("Tem indicação?")
 
-if (dia <= diaMaximo == True) or (indicacao.upper() == "sim" and dia == 29):
+if (dia <= diaMaximo == True) or (indicacao == "Sim" and dia == 29):
     if (idade >= idadeMinima) or (idade <= idadeMaxima):
         if not listaEspera == "sim":
-            print("Inscrição realizada")
+            print("Inscrição realizada\n")
 if listaEspera == "Sim":
-    print("A inscrição não pode ser realizada")
+    print("A inscrição não pode ser realizada\n")
     verf = False
 if idade < idadeMinima:
-    print("Você não tem a idade necessária")
+    print("Você não tem a idade necessária\n")
     verf = False
 if idade > idadeMaxima:
-    print("Possui mais do que a idade indicada")
+    print("Possui mais do que a idade indicada\n")
     verf = False
 if (dia > 28) and (indicacao == "Não"):
-    print("A data de inscrição expirou")
+    print("A data de inscrição expirou\n")
     verf = False
 if verf == False:
     print("Sua incrição foi negada!\n")
