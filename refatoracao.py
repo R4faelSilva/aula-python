0e9aaaa18bad4b2fe0b60f6e8d49c2c0c556b94b
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-09-04 19:49:49 -0300

    Refatoração

---
+++ refatoracao.py
@@ -0,0 +1,14 @@
+## esse exemplo deixa a leitura do código mais facil
+def calc(a , b):
+    return a * b
+x = 10
+y = 20
+resultado = calc(x, y)
+print(resultado)
+##
+def calcular_area(largura, altura):
+    return largura * altura
+largura = 10 
+altura= 20
+area = calcular_area(largura, altura)
+print(area)
