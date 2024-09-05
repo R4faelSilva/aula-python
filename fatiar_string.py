a19735b35a6a5fea8aed5adbbf8c2573a62d10ec
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-09-05 20:57:40 -0300

    Fatiação string

---
+++ fatiar_string.py
@@ -0,0 +1,18 @@
+# Exemplo de string
+texto = "Olá, Mundo!"
+
+# Fatiar do início até um indice específico
+print(texto[:5]) 
+
+# Fatiar a partir de um índice específico até o final
+print(texto[6:])
+
+# Fatiar do início até o final com um passo específico
+print(texto[::2])
+
+# Fatiar com início e fim específicos
+print(texto[3:8])
+
+# Fatiar com passo negativo para inverter uma substring
+print(texto[::-1])
+
