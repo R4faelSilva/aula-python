33412d901f2cba52b4f2034564efd97345e56ead
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-09-04 21:26:55 -0300

    Refatoração 2

--- refatoracao_idade.py
+++ refatoracao_idade.py
@@ -1,12 +1,14 @@
 idade = int(input("Qual a idade?: ")) # Colocar a idade como input para ser possível adicionar qualquer valor
 
-categoria = "Indeinida"
+categoria = "Indefinida"
 if idade < 13:
     categoria = "Criança"
 elif idade >= 13 and idade < 18:
     categoria = "Adolescente"
 elif idade >= 18 and idade < 60:
     categoria = "Adulto"
+elif idade > 100:
+    print("Para de mentir '-'")
 else:
     categoria = "Idoso"
 
