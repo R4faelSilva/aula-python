fb4d77f5577b6d5d4ea101413835bd3ce7c0b3b4
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-09-04 21:04:10 -0300

    Refatoração

--- refatorar_ o _codigo_atividade.py
+++ refatorar_ o _codigo_atividade.py
@@ -1,16 +1,17 @@
 # Variaveis de Preço
-preços = [100,200,300] # coloca os preços em uma lista
-total = sum(preços) # para depois somar todos e dar o valor total
+precosProdutos = [100,200,300] # coloca os preços em uma lista
+precoTotal = sum(precosProdutos) # para depois somar todos e dar o valor total
+valDesconto = 0
 
-desconto = 0
-multiplicadorDesconto = 0.1
+if precoTotal > 500:
+    valDesconto = precoTotal * 0.1 # multiploca o valor totla pelo desconto para dar o valor com o desconto
+    
+else:
+    print(f"Adicione mais itens para um desconto de 10%")
 
-if total > 500: # se o valor total passar de 500 ele aplica um certo desconto
-    valDesconto = total * multiplicadorDesconto # multiploca o valor totla pelo desconto para dar o valor com o desconto
-
-valFinal = total - valDesconto # pega o valor com desconto e subtrai do total
+valFinal = int(precoTotal - valDesconto) # pega o valor com desconto e subtrai do total
 
 # printa a descrição com as variáveis que eu redefini
-print(f"Total antes do desconto: {total}") 
-print(f"Desconto aplicado: {desconto}")
+print(f"Total antes do desconto: {precoTotal}") 
 print(f"Total com desconto: {valFinal}")
+
