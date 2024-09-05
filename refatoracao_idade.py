c6caf846c366da10bbec0ed4c8358ffec191955a
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-09-04 21:49:25 -0300

    Refatoração 2

--- refatoracao_idade.py
+++ refatoracao_idade.py
@@ -1,6 +1,8 @@
 idade = int(input("Qual a idade?: ")) # Colocar a idade como input para ser possível adicionar qualquer valor
 
 categoria = "Indefinida"
+
+# No trecho de condição será validada a idade da persona para analisar sua categoria
 if idade < 13:
     categoria = "Criança"
 elif idade >= 13 and idade < 18:
@@ -13,3 +15,26 @@ else:
     categoria = "Idoso"
 
 print(f"A pessoa é classificada como: {categoria}")
+
+# professor (fez com def)
+def validarIdade(idade):
+    mensagemPadrao = "A pessoa é classificada como: "
+    if idade < 0:
+        return "Essa pessoa ja nasceu????"
+    elif idade > 0 and idade <= 2:
+        return  mensagemPadrao + "Bebê"
+    elif idade > 2 and idade <= 13:
+        return mensagemPadrao + "Criança"
+    elif idade >= 13 and idade < 18:
+        return mensagemPadrao + "Adolescente"
+    elif idade >= 18 and idade < 60:
+        return mensagemPadrao + "Adulto(a)"
+    elif idade > 106:
+        print("Para de mentir '-'")
+    else:
+        return "Idoso"
+# Chamar a variável do def para dar um valor à variável do print 
+categoriaIdade = validarIdade(idade)
+# Usa a variável no print deinida pelo def 
+#print(f"A pessoa é classificada como: {categoriaIdade}")
+print(categoriaIdade)
