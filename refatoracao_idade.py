52b7a62be5ebbb136e6d9d4f17f4ff8e307db080
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-09-04 21:18:50 -0300

    Refatoração 2

--- refatoracao_idade.py
+++ refatoracao_idade.py
@@ -1,9 +1,13 @@
-idade = 25
+idade = int(input("Qual a idade?: ")) # Colocar a idade como input para ser possível adicionar qualquer valor
 
 categoria = "Indeinida"
 if idade < 13:
     categoria = "Criança"
 elif idade >= 13 and idade < 18:
     categoria = "Adolescente"
+elif idade >= 18 and idade < 60:
+    categoria = "Adulto"
+else:
+    categoria = "Idoso"
 
-print("A pessoa é classificada como:", categoria)
+print(f"A pessoa é classificada como: {categoria}")
--- refatorar_ o _codigo_atividade.py
+++ refatorar_ o _codigo_atividade.py
@@ -1,17 +1,15 @@
 # Variaveis de Preço
-precosProdutos = [100,200,300] # coloca os preços em uma lista
+precosProdutos = [100,200,100] # coloca os preços em uma lista
 precoTotal = sum(precosProdutos) # para depois somar todos e dar o valor total
 valDesconto = 0
 
 if precoTotal > 500:
     valDesconto = precoTotal * 0.1 # multiploca o valor totla pelo desconto para dar o valor com o desconto
+    valFinal = int(precoTotal - valDesconto) # pega o valor com desconto e subtrai do total
+    print(f"Total com desconto R$: {valFinal}")
     
 else:
     print(f"Adicione mais itens para um desconto de 10%")
 
-valFinal = int(precoTotal - valDesconto) # pega o valor com desconto e subtrai do total
-
-# printa a descrição com as variáveis que eu redefini
-print(f"Total antes do desconto: {precoTotal}") 
-print(f"Total com desconto: {valFinal}")
-
+# printa a descrição com as variáveis que eu redefini 
+print(f"Total R$: {precoTotal}")
