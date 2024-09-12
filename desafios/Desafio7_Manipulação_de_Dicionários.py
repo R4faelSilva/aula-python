6400505f02859ff87bb1216777283690584aa730
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-09-12 19:57:07 -0300

    Desafios hardcores WoW oh yes

--- desafios/Desafio7_Manipulação_de_Dicionários.py
+++ desafios/Desafio7_Manipulação_de_Dicionários.py
@@ -1,15 +1,7 @@
 alunos = {"Rafael": 8, "Galvão": 10, "Guilherme": 8, "Ryan": 7, "Vinicius": 9.5}
 nome = input("Qual o aluno que você deseja saber a nota? ")
 if nome in alunos:
-    if nome == "Rafael":
-        print("A nota é ", (alunos["Rafael"]))
-    if nome == "Galvão":
-        print("A nota é ", (alunos["Galvão"]))
-    if nome == "Guilherme":
-        print("A nota é ", (alunos["Guilherme"]))
-    if nome == "Ryan":
-        print("A nota é ", (alunos["Ryan"]))
-    if nome == "Vinicius":
-        print("A nota é ", (alunos["Vinicius"]))
+    if nome in alunos:
+        print(f"A nota de {nome} é {alunos[nome]}.")
 else:
     print("O aluno não esta no banco de dados.")
