3c6e06fc0e1e49c8532bdb77e44dedc6f04c2d81
Raf4elSilva <rafaelsilvamachado93@gmail.com>
2024-09-18 21:48:37 -0300

    Desafios hardcores WoW oh yes

--- desafios/Desafio7_Manipulação_de_Dicionários.py
+++ desafios/Desafio7_Manipulação_de_Dicionários.py
@@ -1,7 +1,26 @@
 notas = {"Rafael": 8, "Galvão": 10, "Guilherme": 8, "Ryan": 7, "Vinicius": 9.5}
-nome = input("Qual o aluno que você deseja saber a nota? ")
-if nome in notas:
-    if nome in notas:
-        print(f"A nota de {nome} é {notas[nome]}.")
-else:
-    print("O aluno não esta no banco de dados.")
+
+def recuperarNotaAluno(nome):
+    try:
+        if nome in notas:
+            print(f"A nota de {nome} é {notas[nome]}.")
+        else:
+            raise KeyError
+    except KeyError:
+        print("O aluno não esta no banco de dados.")
+
+        operacao = input("Deseja tentar novametne (S/N)? ")
+        if operacao.upper() == "S":
+            main()
+        elif operacao.upper() == "N":
+            print("Programa encerrado.")
+        else:
+            raise ValueError("Operação não reconhecida")
+        print("O programa irá finalizar.")
+def main():
+    try:
+        nome = input("Qual o aluno que você deseja saber a nota? ")
+        recuperarNotaAluno(nome)
+    except Exception as e:
+        print(f"Ocorreu um erro {e}")
+main()
---
+++ desafios/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+      operacao = input("Deseja tentar novametne (S/N)? ")
+        if operacao.upper() == "S":
+            main()
+        elif operacao.upper() == "N":
+            print("Programa encerrado.")
+        else:
